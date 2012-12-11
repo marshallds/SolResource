fce5f687622e7523df483b7989040e0ef3d00d9b
Marshallds <marshall.ds@gmail.com>
2012-12-10 20:28:03 -0700

    change fuel display to not need mask

--- assets/www/js/interface.js
+++ assets/www/js/interface.js
@@ -40,16 +40,29 @@ var runDiagnostic = function() {
 var setFuel = function(newValue) { // newValue is between 0 and 1
 	if (newValue <=1 && newValue >=0) {
 		var newValuePercent = Math.floor(newValue*100) + '%';
-		var scaledValue = newValue*650;
+		var scaledValue = newValue*646;
 		$('#fuelLabel').text(newValuePercent); //floor or round?
-		if (scaledValue > (140-50)) $('#fuelLabel').css("left", scaledValue + 50)
+		if (scaledValue > 140) $('#fuelLabel').css("left", scaledValue + 50)
 	else $('#fuelLabel').css("left", 140)
-		$('#fuelbar img').css("left", scaledValue + 44)
-		if (newValue >0.15) $('#fuelbar').css({"background":"-webkit-linear-gradient(left,rgba(255,255,255,0.6) " + (scaledValue+67) + ",rgba(255,255,255,0.2) " + (scaledValue+71) + ")"})
-	else $('#fuelbar').css({"background":"-webkit-linear-gradient(left,rgba(255,100,100,0.6) " + (scaledValue+67) + ",rgba(255,150,150,0.2) " + (scaledValue+71) + ")"})
+		$('#fuelbar img').css("left", scaledValue - 40)
+		if (newValue >0.15) $('#fuelbar').css({"background":"-webkit-linear-gradient(left,rgba(255,255,255,0.45) " + scaledValue + ",rgba(255,255,255,0.2) " + scaledValue + ")"})
+	else $('#fuelbar').css({"background":"-webkit-linear-gradient(left,rgba(255,100,100,0.6) " + scaledValue + ",rgba(255,150,150,0.2) " + scaledValue + ")"})
 	}
 
 }
+// var setFuel = function(newValue) { // newValue is between 0 and 1
+// 	if (newValue <=1 && newValue >=0) {
+// 		var newValuePercent = Math.floor(newValue*100) + '%';
+// 		var scaledValue = newValue*650;
+// 		$('#fuelLabel').text(newValuePercent); //floor or round?
+// 		if (scaledValue > (140-50)) $('#fuelLabel').css("left", scaledValue + 50)
+// 	else $('#fuelLabel').css("left", 140)
+// 		$('#fuelbar img').css("left", scaledValue + 44)
+// 		if (newValue >0.15) $('#fuelbar').css({"background":"-webkit-linear-gradient(left,rgba(255,255,255,0.6) " + (scaledValue+67) + ",rgba(255,255,255,0.2) " + (scaledValue+71) + ")"})
+// 	else $('#fuelbar').css({"background":"-webkit-linear-gradient(left,rgba(255,100,100,0.6) " + (scaledValue+67) + ",rgba(255,150,150,0.2) " + (scaledValue+71) + ")"})
+// 	}
+
+// }
 var formatTime = function(seconds) {
 	return Math.floor(seconds/60) + ':' + ('0'+ (seconds % 60)).slice(-2);
 }
