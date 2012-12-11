51398805be05871b4a39ea68d974e385b8aa5da7
Bryan <bryan@newlinetechnicalinnovations.com>
2012-12-10 21:00:58 -0700

    Basic resource and fuel gathering

--- assets/www/js/gameplay.js
+++ assets/www/js/gameplay.js
@@ -1,2 +1,45 @@
 var fuel = 0.70;
-var hydrogen = 
+
+var hydrogen = 20;
+var oxygen = 30;
+var iron = 40;
+var platinum = 10;
+
+//DO INITIAL VARIABLE SHOWING
+setFuel(fuel);
+updatePlayerResources(hydrogen, oxygen, iron, platinum);
+
+function getAsteroidDetails() {
+	updateResources(60,40,10);
+}
+
+function retrieveResources() {
+	if (fuel > 0.01)
+	{		
+		fuel = fuel - 0.01;
+		setFuel(fuel);
+		
+		hydrogen = hydrogen + 40;
+		oxygen = oxygen + 20;
+		iron = iron + 40;
+		platinum = platinum + 10;
+		
+		updatePlayerResources(hydrogen, oxygen, iron, platinum);
+		
+		updateResources(0,0,0);
+		return true;
+	}	
+	else {
+		return false;
+	}
+}
+
+function refuel() {
+	if (hydrogen <= 20 && fuel <= 0.90) {
+		fuel = fuel = 0.01;
+		hydrogen = hydrogen - 20;
+		
+		setFuel(fuel);
+		updatePlayerResources(hydrogen, oxygen, iron, platinum);
+	}
+}
